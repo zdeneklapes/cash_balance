7f33915aa0964109ff9a78058f5000c2f0934d71
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2023-07-16 23:39:22 +0200

    fixed setup.py

---
+++ cash_balance/loan/__init__.py
@@ -0,0 +1,3 @@
+from .loan import calculate_month_to_pay_loan, calculate_year_to_pay_loan
+
+all = ['calculate_month_to_pay_loan', 'calculate_year_to_pay_loan']
--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@ from setuptools import setup, find_packages
 
 directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+  long_description = f.read()
+
 
 setup(
     name='cash_balance',
-    version='0.0.1',
+    version='0.0.2',
     description='Cash balance',
     author='Zdenek Lapes',
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    packages=find_packages(),
+    packages = find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
